024da554bdc1f350e174be1f09f689a5235ae577
Alvaro <alvaroabadbarberan2@gmail.com>
2022-05-13 13:25:09 +0200

    Creando conflicto entre ramas

--- prueba_01_31.sh
+++ prueba_01_31.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 clear
-echo 'segunda mod'
+echo "Soy el script 01"
+#Main: Voy a crear un conflicto entre esta rama y la otra.
+echo "Soy el script 01 con conflicto en la misma linea"
 exit 0
